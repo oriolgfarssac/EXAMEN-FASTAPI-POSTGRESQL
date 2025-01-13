3df62b2c5213ba3abdc8a1794b3269d1cbe469a8
oriol <oriolgfarssac@gmail.com>
2025-01-13 17:31:27 +0100

    segon exercici acabat

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from typing import Optional
+import app
+from MySQLdb import connect, Error
+from fastapi import HTTPException
 from pydantic import BaseModel
 
 class Formulario(BaseModel):
@@ -12,3 +15,42 @@ class Formulario(BaseModel):
     codigo_postal: Optional[int]
     password: str
 
+async def add_user(item: Formulario) -> Optional[dict]:
+    try:
+
+        conn = connect(
+            host="localhost",
+            user="root",
+            password="root",
+            database="mydatabase"
+        )
+
+        cursor = conn.cursor()
+
+        sql = """
+        INSERT INTO Usuaris(nombre, apellido, correo_electronico,
+        descripcion, curso, ano, direccion, codigo_postal, password)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        valores = (
+            item.nombre, item.apellido, item.correo_electronico, item.descripcion,
+            item.curso, item.ano, item.direccion, item.codigo_postal, item.password
+        )
+
+        cursor.execute(sql, valores)
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+        return {"nombre": item.nombre, "apellido": item.apellido}
+    except Error as e:
+        raise Exception(f"Error de base de datos: {str(e)}")
+
+@app.post("/add_user/")
+async def create_user(item: Formulario):
+    try:
+        user = await add_user(item)
+        return {"mensaje": "El usuario se ha creado correctamente", "user": user}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
