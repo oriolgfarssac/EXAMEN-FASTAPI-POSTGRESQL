09f42c0973a89ad433dcb1738d6a85069f02f742
oriol <oriolgfarssac@gmail.com>
2025-01-13 17:52:58 +0100

    tercer exercici acabat

--- main.py
+++ main.py
@@ -15,7 +15,20 @@ class Formulario(BaseModel):
     codigo_postal: Optional[int]
     password: str
 
-async def add_user(item: Formulario) -> Optional[dict]:
+class UserSchema(BaseModel):
+    nombre: str
+    apellido: str
+    correo_electronico: str
+    descripcion: Optional[str]
+    curso: str
+    ano: int
+    codigo_postal: Optional[int]
+
+# Camps que considero sensibles i que no s'inclouren a l'Schema:
+# - direccion
+# - password
+
+async def add_user(item: Formulario) -> UserSchema:
     try:
 
         conn = connect(
@@ -28,7 +41,7 @@ async def add_user(item: Formulario) -> Optional[dict]:
         cursor = conn.cursor()
 
         sql = """
-        INSERT INTO Usuaris(nombre, apellido, correo_electronico,
+        INSERT INTO Usuarios(nombre, apellido, correo_electronico,
         descripcion, curso, ano, direccion, codigo_postal, password)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
@@ -43,7 +56,15 @@ async def add_user(item: Formulario) -> Optional[dict]:
         cursor.close()
         conn.close()
 
-        return {"nombre": item.nombre, "apellido": item.apellido}
+        return UserSchema(
+            nombre=item.nombre,
+            apellido=item.apellido,
+            correo_electronico=item.correo_electronico,
+            descripcion=item.descripcion,
+            curso=item.curso,
+            ano=item.ano,
+            codigo_postal=item.codigo_postal
+        )
     except Error as e:
         raise Exception(f"Error de base de datos: {str(e)}")
 
